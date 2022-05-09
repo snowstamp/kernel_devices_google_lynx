eb9951c80f7eb0c11f8233e9686047ba9ea7c34e
Robin Peng <robinpeng@google.com>
2022-05-09 09:58:51 +0800

    update_symbol_list.sh: update lynx build files
    * update_symbol_list_lynx-aosp.sh for aosp kernel
    * update_symbol_list_lynx-staging.sh for staging kernel
    
    Bug: 230573746
    Signed-off-by: Robin Peng <robinpeng@google.com>
    Change-Id: Ifb0ab51791e40f11fefd45d6f00082aebbf9d111

--- update_symbol_list_lynx-aosp.sh
+++ update_symbol_list_lynx-aosp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # SPDX-License-Identifier: GPL-2.0
+BUILD_SCRIPT="./build_lynx.sh" \
 DEVICE_KERNEL_BUILD_CONFIG=private/devices/google/lynx/build.config.lynx \
 private/gs-google/update_symbol_list.sh "$@"
