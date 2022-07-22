624f0eb3a93a9fa72d78063c577332b8a19d986d
Amy Hsu <amyhsu@google.com>
2022-07-22 11:10:16 +0800

    panel: s6e3fc5: Modify max brightness to FFE (4094)
    LHBM is too bright in high brightness mode(GHBM) in P1.1 and EVT1.0 panel.
    The symptom only happened in DBV=FFF. LHBM brightness is good if DBV≦FFE.
    So add this workaround for proto, EVT1.0 to resolve it.
    
    Bug: 239140157
    Test: adb shell vndservice call Exynos.HWCService 1002 i32 0 f 1.0
          Then check kernel debug log for the brightness value
    
    Signed-off-by: Amy Hsu <amyhsu@google.com>
    Change-Id: I3edabbc9fbda4c24cc17d2e1e6fbcb7a370c44de

--- display/panel-samsung-s6e3fc5.c
+++ display/panel-samsung-s6e3fc5.c
@@ -440,6 +440,31 @@ static void s6e3fc5_update_wrctrld(struct exynos_panel *ctx)
 	EXYNOS_DCS_WRITE_SEQ(ctx, MIPI_DCS_WRITE_CONTROL_DISPLAY, val);
 }
 
+#define MAX_BR_HBM_EVT1 4094
+static int s6e3fc5_set_brightness(struct exynos_panel *ctx, u16 br)
+{
+	u16 brightness;
+
+	if (ctx->current_mode->exynos_mode.is_lp_mode) {
+		const struct exynos_panel_funcs *funcs;
+
+		funcs = ctx->desc->exynos_panel_func;
+		if (funcs && funcs->set_binned_lp)
+			funcs->set_binned_lp(ctx, br);
+		return 0;
+	}
+
+	if (ctx->panel_rev <= PANEL_REV_EVT1 && br >= MAX_BR_HBM_EVT1) {
+		br = MAX_BR_HBM_EVT1;
+		dev_dbg(ctx->dev, "%s: capped to dbv(%d) for EVT1 and before\n",
+			__func__, MAX_BR_HBM_EVT1);
+	}
+
+	brightness = (br & 0xff) << 8 | br >> 8;
+
+	return exynos_dcs_set_brightness(ctx, brightness);
+}
+
 static void s6e3fc5_set_nolp_mode(struct exynos_panel *ctx,
 				  const struct exynos_panel_mode *pmode)
 {
@@ -772,7 +797,7 @@ static const struct drm_panel_funcs s6e3fc5_drm_funcs = {
 };
 
 static const struct exynos_panel_funcs s6e3fc5_exynos_funcs = {
-	.set_brightness = exynos_panel_set_brightness,
+	.set_brightness = s6e3fc5_set_brightness,
 	.set_lp_mode = exynos_panel_set_lp_mode,
 	.set_nolp_mode = s6e3fc5_set_nolp_mode,
 	.set_binned_lp = exynos_panel_set_binned_lp,
