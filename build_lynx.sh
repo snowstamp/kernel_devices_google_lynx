7cc7c364afd4d7563b1cb7b65e43bd7598374f55
maade93791 <70593890+maade69@users.noreply.github.com>
2024-12-25 21:20:56 +0300

    sign all modules instead of only GKI modules

--- build_lynx.sh
+++ build_lynx.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # SPDX-License-Identifier: GPL-2.0
 
-exec tools/bazel run \
+test -d out/ && rm -rf out/
+
+tools/bazel run \
     --config=stamp \
     --config=lynx \
-    //private/devices/google/lynx:gs201_lynx_dist "$@"
+    //private/devices/google/lynx:gs201_lynx_dist "$@" && DEVICE=lynx private/devices/google/common/sign_kernel_modules.sh
